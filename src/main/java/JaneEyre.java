dde795f496d1c8717fe56e16277479fa6aa52146
Anna Tan <e1356946@u.nus.edu>
2026-01-15 13:21:39 +0800

    renamed bot

---
+++ src/main/java/JaneEyre.java
@@ -0,0 +1,8 @@
+public class JaneEyre {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________\n";
+        System.out.println(line + "Hello, I'm Jane Eyre\n");
+        System.out.println("What can I do for you?\n" + line);
+        System.out.println("Bye. Hope to see you again soon!\n" + line);
+    }
+}
