4265799b78f355e1f50a717c8687562cea10b102
Anna Tan <e1356946@u.nus.edu>
2026-01-15 13:33:58 +0800

    bot echoes user input and exits on command bye

--- src/main/java/JaneEyre.java
+++ src/main/java/JaneEyre.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
 public class JaneEyre {
     public static void main(String[] args) {
         String line = "____________________________________________________________\n";
         System.out.println(line + "Hello, I'm Jane Eyre\n");
         System.out.println("What can I do for you?\n" + line);
+
+        Scanner scanner = new Scanner(System.in);
+        boolean bye = false;
+        while (!bye) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                bye = true;
+            } else {
+                System.out.println(line + input + "\n" + line);
+            }
+        }
         System.out.println("Bye. Hope to see you again soon!\n" + line);
     }
 }
