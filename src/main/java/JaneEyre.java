54bf5b63cea31852c03ae4baf243f0cd4ed034d5
Anna Tan <e1356946@u.nus.edu>
2026-01-16 09:29:44 +0800

    Added the ability to store tasks entered by user and store them back to the user in list format when requested

--- src/main/java/JaneEyre.java
+++ src/main/java/JaneEyre.java
@@ -1,20 +1,32 @@
 import java.util.Scanner;
 public class JaneEyre {
+    private static final String LINE = "____________________________________________________________\n";
+    public static void taskListPrinter(String[] tasks, int numTasks) {
+        for (int i = 0; i < numTasks; i++) {
+            System.out.println(String.format("%d. %s", i + 1, tasks[i]));
+        }
+        System.out.println(LINE);
+    }
     public static void main(String[] args) {
-        String line = "____________________________________________________________\n";
-        System.out.println(line + "Hello, I'm Jane Eyre\n");
-        System.out.println("What can I do for you?\n" + line);
+        System.out.println(LINE + "Hello, I'm Jane Eyre\n");
+        System.out.println("What can I do for you?\n" + LINE);
 
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int idx = 0;
         boolean isDone = false;
         while (!isDone) {
-            String input = scanner.nextLine();
+            String input = scanner.nextLine().trim();
             if (input.equals("bye")) {
                 isDone = true;
+            } else if (input.equals("list")) {
+                taskListPrinter(tasks, idx);
             } else {
-                System.out.println(line + input + "\n" + line);
+                System.out.println(LINE + "added: " + input + "\n" + LINE);
+                tasks[idx] = input;
+                idx += 1;
             }
         }
-        System.out.println("Bye. Hope to see you again soon!\n" + line);
+        System.out.println("Bye. Hope to see you again soon!\n" + LINE);
     }
 }
