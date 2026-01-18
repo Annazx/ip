88449972495ae81eadd08840be78f84468a06447
Anna Tan <e1356946@u.nus.edu>
2026-01-18 13:49:44 +0800

    Added support for Todos, Deadlines, and Events

--- src/main/java/JaneEyre.java
+++ src/main/java/JaneEyre.java
@@ -1,42 +1,70 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 public class JaneEyre {
-    private static final String LINE = "____________________________________________________________\n";
-    public static void taskListPrinter(Task[] tasks, int numTasks) {
-        for (int i = 0; i < numTasks; i++) {
-            System.out.println(String.format("%d. %s", i + 1, tasks[i]));
+    private static final String LINE =
+            "____________________________________________________________\n";
+    public static void taskListPrinter(ArrayList<Task> tasks) {
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(String.format("%d. %s", i + 1, tasks.get(i)));
         }
         System.out.println(LINE);
     }
+
+    public static void handleMark(String input, ArrayList<Task> tasks) {
+        int i = Integer.valueOf(input.substring(4).trim()) - 1;
+        // what if it is an error?
+        tasks.set(i, tasks.get(i).mark(true));
+        System.out.println(LINE + "Nice! I've marked this task as done:\n  "
+                + tasks.get(i) + "\n" + LINE);
+    }
+
+    public static void handleUnmark(String input, ArrayList<Task> tasks) {
+        int i = Integer.valueOf(input.substring(6).trim()) - 1;
+        // need exception handling here
+        tasks.set(i, tasks.get(i).mark(false));
+        System.out.println(LINE + "OK, I've marked this task as not done yet:\n  "
+                + tasks.get(i) + "\n" + LINE);
+    }
+
+    public static void handleAdd(String input, ArrayList<Task> tasks) {
+        System.out.println(LINE + "added: " + input + "\n" + LINE);
+        if (input.contains("/from") && input.contains("/to")) {
+            int start = input.indexOf("/from");
+            int end = input.indexOf("/to");
+            //System.out.println(String.format("%d %d", start, end));
+            String des = input.substring(0, start).trim();
+            String from = input.substring(5 + start, end).trim();
+            String to = input.substring(end + 3).trim();
+            tasks.add(new Event(des, from, to));
+        } else if (input.contains("/by")) {
+            int start = input.indexOf("/by");
+            String des = input.substring(0, start).trim();
+            String by = input.substring(start + 3).trim();
+            tasks.add(new Deadline(des, by));
+        } else {
+            tasks.add(new Todo(input));
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println(LINE + "Hello, I'm Jane Eyre\n");
         System.out.println("What can I do for you?\n" + LINE);
 
         Scanner scanner = new Scanner(System.in);
-        Task[] tasks = new Task[100];
-        int idx = 0;
+        ArrayList<Task> tasks = new ArrayList<>(100);
         boolean isDone = false;
         while (!isDone) {
             String input = scanner.nextLine().trim();
             if (input.equals("bye")) {
                 isDone = true;
             } else if (input.equals("list")) {
-                taskListPrinter(tasks, idx);
-            } else if (input.indexOf("mark") == 0) {
-                int i = Integer.valueOf(input.substring(4).trim()) - 1;
-                // what if it is an error?
-                tasks[i] = tasks[i].mark(true);
-                System.out.println(LINE + "Nice! I've marked this task as done:\n  "
-                        + tasks[i] + "\n" + LINE);
-            } else if (input.indexOf("unmark") == 0) {
-                int i = Integer.valueOf(input.substring(6).trim()) - 1;
-                // need exception handling here
-                tasks[i] = tasks[i].mark(false);
-                System.out.println(LINE + "OK, I've marked this task as not done yet:\n  "
-                        + tasks[i] + "\n" + LINE);
+                taskListPrinter(tasks);
+            } else if (input.startsWith("mark")) {
+                handleMark(input, tasks);
+            } else if (input.startsWith("unmark")) {
+                handleUnmark(input, tasks);
             } else {
-                System.out.println(LINE + "added: " + input + "\n" + LINE);
-                tasks[idx] = new Task(input);
-                idx += 1;
+                handleAdd(input, tasks);
             }
         }
         System.out.println("Bye. Hope to see you again soon!\n" + LINE);
